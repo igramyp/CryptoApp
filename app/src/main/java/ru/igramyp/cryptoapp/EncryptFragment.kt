a9adcb3c5fc3bc92822a7da354d51adca2676864
Igramyp <igramyp@gmail.com>
2024-08-05 16:07:52 +0300

    add EncryptFragment

--- app/src/main/java/ru/igramyp/cryptoapp/EncryptFragment.kt
+++ app/src/main/java/ru/igramyp/cryptoapp/EncryptFragment.kt
@@ -5,8 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.findNavController
+
 
 class EncryptFragment : Fragment() {
 
