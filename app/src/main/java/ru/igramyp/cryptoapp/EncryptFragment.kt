8078d0f6405c6fc98f02d8c0264b92d6cd86d411
Igramyp <igramyp@gmail.com>
2024-08-09 16:18:17 +0300

    fix error in fragment_message.xml

--- app/src/main/java/ru/igramyp/cryptoapp/EncryptFragment.kt
+++ app/src/main/java/ru/igramyp/cryptoapp/EncryptFragment.kt
@@ -12,9 +12,8 @@ class EncryptFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-       val view = inflater.inflate(R.layout.fragment_encrypt, container, false)
+        savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_encrypt, container, false)
         val message = EncryptFragmentArgs.fromBundle(requireArguments()).message
         val encryptedView = view.findViewById<TextView>(R.id.encrypted_message)
         encryptedView.text = message.reversed()
